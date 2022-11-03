b0664a3d503af5c4b8d2c497eae1db199ed2b224
FrankBonanno <fb0023@mix.wvu.edu>
2022-11-03 01:25:07 -0400

    tab icon and file structure

--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 1 meter = 3.281 feet
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
+
+REQS:
+1) Follow design spec (On Figma)
+2) Generate all conversion when the user clicks "Convert"
+3) Round the numbers down to 3 places use number.toFixed(3) for this
 */
 console.log('Hello World!');
